13fe262a8880327446766d3167b0510dc1ced74a
Tathagata Basu <tathagatabasu.basu@gmail.com>
2015-09-09 12:05:48 +0530

    Initial SQL File cleanup

--- smartcatadmin.sql
+++ smartcatadmin.sql
@@ -4,7 +4,7 @@ Navicat MySQL Data Transfer
 Source Server         : localhost
 Source Server Version : 50536
 Source Host           : localhost:3306
-Source Database       : supr_admin
+Source Database       : smartcatadmin
 
 Target Server Type    : MYSQL
 Target Server Version : 50536
