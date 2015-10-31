9fc40d9609016463506d5a260de8eecbcdd0e86a
Tathagata Basu <tathagatabasu.basu@gmail.com>
2015-10-31 11:11:37 +0530

    Fix - Master Breadcrumb

--- master.php
+++ master.php
@@ -22,7 +22,7 @@
 <h3>Master Page</h3>
 <ul class="breadcrumb">
   <li>You are here:</li>
-  <li> <a href="#" class="tip" title="back to dashboard"> <span class="icon16 icomoon-icon-screen-2"></span> </a> <span class="divider"> <span class="icon16 icomoon-icon-arrow-right-2"></span> </span> </li>
+  <li> <a href="dashboard.html" class="tip" title="Dashboard"> <span class="icon16 icomoon-icon-screen-2"></span> </a> <span class="divider"> <span class="icon16 icomoon-icon-arrow-right-2"></span> </span> </li>
   <li class="active">Master Page</li>
 </ul>
 </div>
